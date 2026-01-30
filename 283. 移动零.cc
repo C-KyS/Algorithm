8f94643ef4dc060a686b44039539452bddf25f01
CYH-KennyS <1912521148@qq.com>
2026-01-30 22:35:13 +0800

    add code

---
+++ 283. 移动零.cc
@@ -0,0 +1,29 @@
+// 题目链接: https://leetcode.cn/problems/move-zeroes/description/
+
+
+class Solution {
+public:
+    void exchange(int &a, int &b)
+    {
+        int tmp = a;
+        a=b;
+        b=tmp; 
+    }
+
+    void moveZeroes(vector<int>& nums) {
+        int des = -1; // 初始des为-1, 即cur-1
+        for(int cur = 0; cur < nums.size(); cur++)
+        {
+            
+            if(nums[cur] == 0)
+            {   // 当前处理数字为0, 直接纳入不作多余处理
+                continue;
+            }
+            else
+            {   // 当前处理数字为非0, 将当前数组第一个0与其对换位置，再纳入
+                exchange(nums[des+1], nums[cur]);
+                des++;
+            }
+        }
+    }
+};
