d74b5b5438fcefd3562d6b2ceb73e7e2e0ce9ae6
CYH-KennyS <1912521148@qq.com>
2026-02-01 00:47:28 +0800

    update

---
+++ 1089. 复写零.cc
@@ -0,0 +1,54 @@
+class Solution
+{
+public:
+    // 1. 查找最后元素cur, 及des(一般固定为n-1, 特殊情况下不为n-1, 因此des也会变化)
+    int findLastCur(vector<int> &arr, int &input_des)
+    {
+        int n = arr.size();
+        int cur = 0;
+        int des = -1;
+        while (cur < n)
+        {
+            if (arr[cur] != 0)
+                des++;
+            else
+                des += 2;
+
+            if (des == n)
+            {
+                arr[arr.size() - 1] = 0;
+                des -= 2;
+                cur--;
+                break;
+            }
+            if (des == n - 1)
+                break;
+
+            cur++;
+        }
+
+        input_des = des;
+        return cur;
+    }
+
+    void duplicateZeros(vector<int> &arr)
+    {
+        int des = -1;
+        // 1. 查找最后元素cur, 及des(一般固定为n-1, 特殊情况下不为n-1, 因此des也会变化)
+        int cur = findLastCur(arr, des);
+        // cout << cur <<" & " << des;
+        // 2. cur, des已确定，从后向前逐渐覆盖
+        while (cur >= 0)
+        {
+            if (arr[cur] != 0)
+                // cur位置非零, des只覆盖一步
+                arr[des--] = arr[cur--];
+            else
+            { // cur位置为零, des覆盖两步
+                arr[des--] = 0;
+                arr[des--] = 0;
+                cur--;
+            }
+        }
+    }
+};
